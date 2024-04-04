29e950ed6cfde3ed71e19f1a37f507c68a4aa252
Nash <141100397+s2an@users.noreply.github.com>
2024-04-04 17:20:34 -0400

    fdshjk

--- lib/hype_man.rb
+++ lib/hype_man.rb
@@ -11,40 +11,40 @@ require 'shellwords'
 
 module HypeMan
 
-  # def self.set_alias(alias_name, command)
-  #   if ENV['SHELL'] == '/bin/zsh'
-  #     if command || alias_name == "bundle exec rspec"
-  #       system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
-  #       puts "Way to test bro"
-  #     elsif command || alias_name == "rails db:drop"
-  #       system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
-  #       puts "do you even know how to database bro??"
-  #       # `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
-  #     elsif
-  #       system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
-  #       # `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
-  #       puts "escape test"
-  #     end
-  #   else
-  #     puts "Unsupported shell. Alias not set."
-  #   end
-  # end
-  
-  def self.hype_hype(command)
+  def self.set_alias(alias_name, command)
     if ENV['SHELL'] == '/bin/zsh'
-      if command == "bundle exec rspec"
+      if command || alias_name == "bundle exec rspec"
+        system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
         puts "Way to test bro"
-      elsif
-        command == "rails db:drop"
+      elsif command || alias_name == "rails db:drop"
+        system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
         puts "do you even know how to database bro??"
-      else
-        command == "git log"
-        puts "What do you want to see bro..."
+        # `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
+      elsif
+        system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
+        # `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
+        puts "escape test"
       end
     else
-      puts "II: Unsupported shell."
+      puts "Unsupported shell. Alias not set."
     end
   end
+  
+  # def self.hype_hype(command)
+  #   if ENV['SHELL'] == '/bin/zsh'
+  #     if command == "bundle exec rspec"
+  #       puts "Way to test bro"
+  #     elsif
+  #       command == "rails db:drop"
+  #       puts "do you even know how to database bro??"
+  #     else
+  #       command == "git log"
+  #       puts "What do you want to see bro..."
+  #     end
+  #   else
+  #     puts "II: Unsupported shell."
+  #   end
+  # end
 
 end
 
