35b296136925389fdb06ea0703fef996021fc2c3
Nash <141100397+s2an@users.noreply.github.com>
2024-04-04 16:17:53 -0400

    Just puts

--- lib/hype_man.rb
+++ lib/hype_man.rb
@@ -11,20 +11,38 @@ require 'shellwords'
 
 module HypeMan
 
-  def self.set_alias(alias_name, command)
+  # def self.set_alias(alias_name, command)
+  #   if ENV['SHELL'] == '/bin/zsh'
+  #     if command || alias_name == "bundle exec rspec"
+  #       system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
+  #       puts "Way to test bro"
+  #     elsif command || alias_name == "rails db:drop"
+  #       system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
+  #       puts "do you even know how to database bro??"
+  #       # `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
+  #     elsif
+  #       system("echo \"alias #{alias_name}='#{Shellwords.escape(command)}'\" >> ~/.zshrc")
+  #       # `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
+  #       puts "escape test"
+  #     end
+  #   else
+  #     puts "Unsupported shell. Alias not set."
+  #   end
+  # end
+  
+  def self.hype_hype(command)
     if ENV['SHELL'] == '/bin/zsh'
-      if command || alias_name == "bundle exec rspec"
-        `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
+      if command == "bundle exec rspec"
         puts "Way to test bro"
-      elsif command || alias_name == "rails db:drop"
-        `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
-        "do you even know how to database bro??"
       elsif
-      `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
-      puts "escape test"
+        command == "rails db:drop"
+        puts "do you even know how to database bro??"
+      elsif
+        command == "git log"
+        puts "What do you want to see bro..."
       end
     else
-      puts "Unsupported shell. Alias not set."
+      puts "II: Unsupported shell."
     end
   end
 
--- spec/hype_man_spec.rb
+++ spec/hype_man_spec.rb
@@ -1,3 +1,5 @@
+require "spec_helper"
+require "hype_man"
 # frozen_string_literal: true
 
 RSpec.describe HypeMan do
@@ -7,5 +9,57 @@ RSpec.describe HypeMan do
 
   xit "does something useful" do
     expect(false).to eq(true)
+  end
+
+  describe '.set_alias' do
+    context 'when the shell is Zsh' do
+      before do
+        allow(ENV).to receive(:[]).with('SHELL').and_return('/bin/zsh')
+      end
+
+      it 'sets the alias for "bundle exec rspec"' do
+        expect { HypeMan.set_alias('bundle exec rspec', 'bundle exec rspec') }.to output("Way to test bro\n").to_stdout
+      end
+
+      it 'sets the alias for "rails db:drop"' do
+        expect { HypeMan.set_alias('rails db:drop', 'rails db:drop') }.to output("do you even know how to database bro??\n").to_stdout
+      end
+
+      it 'sets the alias and outputs "escape test"' do
+        expect { HypeMan.set_alias('custom_alias', 'custom_command') }.to output("escape test\n").to_stdout
+      end
+    end
+
+    context 'when the shell is not supported' do
+      before do
+        allow(ENV).to receive(:[]).with('SHELL').and_return('/bin/unsupported')
+      end
+
+      it 'outputs "Unsupported shell. Alias not set."' do
+        expect { HypeMan.set_alias('custom_alias', 'custom_command') }.to output("Unsupported shell. Alias not set.\n").to_stdout
+      end
+    end
+  end
+
+  describe '.hype_hype' do
+    context 'when the shell is Zsh' do
+      before do
+        allow(ENV).to receive(:[]).with('SHELL').and_return('/bin/zsh')
+      end
+
+      it 'appends the command to ~/.zshrc' do
+        expect { HypeMan.hype_hype('custom_command') }.not_to raise_error
+      end
+    end
+
+    context 'when the shell is not supported' do
+      before do
+        allow(ENV).to receive(:[]).with('SHELL').and_return('/bin/unsupported')
+      end
+
+      it "outputs II: Unsupported shell" do
+        expect { HypeMan.hype_hype('custom_command') }.to output("II: Unsupported shell.\n").to_stdout
+      end
+    end
   end
 end
