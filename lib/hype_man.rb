384c3fec46de6b196449c50082fc7d833269094a
Nash <141100397+s2an@users.noreply.github.com>
2024-04-04 11:53:47 -0400

    Update .gemspec

--- lib/hype_man.rb
+++ lib/hype_man.rb
@@ -6,3 +6,18 @@ module HypeMan
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require 'shellwords'
+
+module MyTerminalAlias
+  def self.set_alias(alias_name, command)
+    if ENV['SHELL'] == '/bin/zsh'
+      `echo "alias #{alias_name}='#{Shellwords.escape(command)}'" >> ~/.zshrc`
+      puts "Hello World"
+    else
+      puts "Unsupported shell. Alias not set."
+    end
+  end
+end
+
+MyTerminalAlias.set_alias(bers, bundle_exec_rspec)
